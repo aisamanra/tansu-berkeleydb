14309c08e13070567a8e18d8beab5ce7af674ade
Getty Ritter <gettylefou@gmail.com>
2016-01-31 17:00:57 -0800

    Renamed Database->TansuDB

--- Database/Tansu/Backend/BerkeleyDb.hs
+++ Database/Tansu/Backend/BerkeleyDb.hs
@@ -4,7 +4,7 @@ import Database.Berkeley.Db
 import Data.ByteString (ByteString)
 import System.Directory (createDirectoryIfMissing)
 
-import Database.Tansu.Internal (Database(..))
+import Database.Tansu.Internal (TansuDb(..))
 
 bdbSet :: Db -> ByteString -> ByteString -> IO ()
 bdbSet db key val =
@@ -14,17 +14,17 @@ bdbGet :: Db -> ByteString -> IO (Maybe ByteString)
 bdbGet db key =
   db_get [] db Nothing key
 
-withBerkeleyDb :: FilePath -> (Database -> IO a) -> IO a
+withBerkeleyDb :: FilePath -> (TansuDb -> IO a) -> IO a
 withBerkeleyDb path comp = do
   createDirectoryIfMissing True path
   env <- dbEnv_create []
   dbEnv_open [DB_CREATE,DB_INIT_MPOOL,DB_INIT_TXN] 0 env path
   db <- db_create [] env
   db_open [DB_CREATE] DB_HASH 0 db Nothing path Nothing
-  result <- comp $ Database { dbGet = bdbGet db
-                            , dbSet = bdbSet db
-                            , dbRunTransaction = id
-                            }
+  result <- comp $ TansuDb { dbGet = bdbGet db
+                           , dbSet = bdbSet db
+                           , dbRunTransaction = id
+                           }
   db_close [] db
   dbEnv_close [] env
   return result
