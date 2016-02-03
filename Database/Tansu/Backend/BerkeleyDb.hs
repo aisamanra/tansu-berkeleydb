f9d1a2d3285aaff88ad58a96688facf55d6e4d3e
Getty Ritter <gdritter@galois.com>
2016-02-03 13:19:36 -0800

    Updated bdb backend for new tansu backend API

--- Database/Tansu/Backend/BerkeleyDb.hs
+++ Database/Tansu/Backend/BerkeleyDb.hs
@@ -1,27 +1,39 @@
 module Database.Tansu.Backend.BerkeleyDb
          (withBerkeleyDb) where
 
+import Control.Exception (catch)
 import Database.Berkeley.Db
 import Data.ByteString (ByteString)
 import System.Directory (createDirectoryIfMissing)
 
-import Database.Tansu.Internal (TansuDb(..))
+import Database.Tansu.Internal
 
-bdbSet :: Db -> ByteString -> ByteString -> IO ()
+catchIO :: IO a -> IO (Either TansuError a)
+catchIO mote = fmap return mote `catch` go
+  where go :: DbException -> IO (Either TansuError a)
+        go = return . Left . OtherError . show
+
+bdbSet :: Db -> ByteString -> ByteString -> IO (Either TansuError ())
 bdbSet db key val =
-  db_put [] db Nothing key val
+  catchIO $ db_put [] db Nothing key val
 
-bdbGet :: Db -> ByteString -> IO (Maybe ByteString)
-bdbGet db key =
-  db_get [] db Nothing key
+bdbGet :: Db -> ByteString -> IO (Either TansuError ByteString)
+bdbGet db key = do
+  rs <- catchIO $ db_get [] db Nothing key
+  case rs of
+    Right Nothing  -> return (Left (KeyNotFound key))
+    Right (Just x) -> return (return x)
+    Left err       -> return (Left err)
 
-bdbDel :: Db -> ByteString -> IO ()
+bdbDel :: Db -> ByteString -> IO (Either TansuError ())
 bdbDel db key =
-  db_del [] db Nothing key
+  catchIO $ db_del [] db Nothing key
 
 -- | Open or create a database at the supplied path
---   using the BerkeleyDB library.
-withBerkeleyDb :: FilePath -> (TansuDb -> IO a) -> IO a
+--   using the BerkeleyDB library. Right now, this uses
+--   a consistent set of options, but probably should
+--   become configurable at some point.
+withBerkeleyDb :: FilePath -> (TansuDb k v -> IO a) -> IO a
 withBerkeleyDb path comp = do
   createDirectoryIfMissing True path
   env <- dbEnv_create []
